ce4470e8ba429403d8b757a97e902991be0653b6
s4paneum <45764527+s4paneum@users.noreply.github.com>
2022-12-22 10:18:17 +0100

    implement buggy features

--- Assets/Scripts/CityAR/VisualizationCreator.cs
+++ Assets/Scripts/CityAR/VisualizationCreator.cs
@@ -1,5 +1,11 @@
-﻿using DefaultNamespace;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DefaultNamespace;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace CityAR
 {
@@ -7,9 +13,13 @@ namespace CityAR
     {
 
         public GameObject districtPrefab;
+        public GameObject buildingPrefab;
         private DataObject _dataObject;
         private GameObject _platform;
         private Data _data;
+        private Dictionary<GameObject, Entry> buildings = new Dictionary<GameObject, Entry>();
+
+        public int rows = 3;
 
         private void Start()
         {
@@ -39,6 +49,65 @@ namespace CityAR
             if (entry.type.Equals("File"))
             {
                 //TODO if entry is from type File, create building
+
+                entry.color = Color.white;
+                entry.w = 0.5f;
+                entry.h = 0.5f;
+                
+                // create base, if the parent has no prefab
+                if (entry.parentEntry.goc == null)
+                {
+                    GameObject prefabInstance = Instantiate(districtPrefab, _platform.transform, true);
+                    prefabInstance.name = entry.parentEntry.name + "Base2";
+                    GridObjectCollection grid= prefabInstance.AddComponent(typeof(GridObjectCollection)) as GridObjectCollection;
+                    grid.Rows = 3;
+                    grid.CellWidth = 0.2f;
+                    entry.parentEntry.goc = grid;
+                    prefabInstance.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+                    prefabInstance.transform.localScale = new Vector3(entry.parentEntry.w, 1f,entry.parentEntry.h);
+                    prefabInstance.transform.localPosition = new Vector3(entry.parentEntry.x, entry.parentEntry.deepth+0.001f, entry.parentEntry.z);
+                }
+                
+                // create building
+                GameObject building = Instantiate(buildingPrefab, entry.parentEntry.goc.transform, false);
+                building.name = entry.name;
+                building.transform.GetChild(0).rotation = Quaternion.Euler(90,0,0);
+                building.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = entry.color;
+                float loc = entry.numberOfLines / 50;
+                //float scale_w = entry.w / entry.parentEntry.goc.Rows;
+                float scale_w = 0.15f;
+                //float scale_h = entry.h / entry.parentEntry.files.Count / entry.parentEntry.goc.Rows;
+                float scale_h = 0.15f;
+                int i = 0;
+                foreach (Entry e in entry.parentEntry.files)
+                {
+                    if (e == entry.parentEntry.files[i])
+                    {
+                        break;
+                    }
+
+                    i++;
+                }
+                float x = entry.parentEntry.x + i * scale_w * entry.parentEntry.files.Count / rows;
+                float z = entry.parentEntry.z + i * scale_h * (entry.parentEntry.files.Count % rows);
+                building.transform.localScale = new Vector3(scale_w, loc,scale_h);
+                building.transform.localPosition = new Vector3(x, entry.parentEntry.deepth+0.001f, z);
+                entry.parentEntry.goc.UpdateCollection();
+                buildings.Add(building, entry);
+
+                GameObject tooltip = building.transform.GetChild(1).gameObject;
+                building.transform.GetChild(1).GetComponent<ToolTip>().ToolTipText =
+                    "Name: " + entry.name + "\n loc: " + entry.numberOfLines;
+               
+                Vector3 parentScale = tooltip.transform.parent.localScale;
+                Vector3 scale = tooltip.transform.localScale;
+                tooltip.transform.localScale = new Vector3 (scale.x / parentScale.x, scale.y / parentScale.y, scale.z / parentScale.z);
+    
+                Vector3 posTmp = tooltip.transform.localPosition;
+                posTmp.x = tooltip.transform.localPosition.x;
+                posTmp.y = building.transform.localPosition.y;
+                posTmp.z = tooltip.transform.localPosition.z;
+                tooltip.transform.localPosition = posTmp;
             }
             else
             {
@@ -85,7 +154,7 @@ namespace CityAR
                         entry.h = 1f - z;
                     }
                     entry.deepth += 1;
-                    BuildDistrictBlock(entry, true);
+                    //BuildDistrictBlock(entry, true);
                 }
                 else
                 {
@@ -96,7 +165,7 @@ namespace CityAR
                         entry.w = 1f - x;
                     }
                     entry.deepth += 1;
-                    BuildDistrictBlock(entry, true);
+                    //BuildDistrictBlock(entry, true);
                 }
             }
         }
@@ -128,11 +197,11 @@ namespace CityAR
                 }
                 else
                 {
-                    prefabInstance.name = entry.name+"Base";
+                    prefabInstance.name = entry.name + "Base";
                     prefabInstance.transform.GetChild(0).rotation = Quaternion.Euler(90,0,0);
-                    prefabInstance.transform.localScale = new Vector3(entry.w, 1,entry.h);
+                    //prefabInstance.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = entry.color;
+                    prefabInstance.transform.localScale = new Vector3(entry.w, 1f,entry.h);
                     prefabInstance.transform.localPosition = new Vector3(entry.x, entry.deepth+0.001f, entry.z);
-
                 }
                 
                 Vector3 scale = prefabInstance.transform.localScale;
@@ -143,7 +212,7 @@ namespace CityAR
                 prefabInstance.transform.localScale = new Vector3(scaleX, scale.y, scaleZ);
                 Vector3 position = prefabInstance.transform.localPosition;
                 prefabInstance.transform.localPosition = new Vector3(position.x - shiftX, position.y, position.z + shiftZ);
-            }
+            }  
         }
 
         private bool ContainsDirs(Entry entry)
@@ -186,5 +255,68 @@ namespace CityAR
 
             return color;
         }
+        
+        public void onButtonPressed(string button)
+        {
+            foreach (KeyValuePair<GameObject,Entry> pair in buildings)
+            {
+                GameObject building = pair.Key;
+                Entry entry = pair.Value;
+                GameObject tooltip = building.transform.GetChild(1).gameObject;
+                
+                var color = Color.white;
+                float metric = 1f;
+                
+                if (button.Equals("loc"))
+                {
+                    color = Color.white;
+                    metric = entry.numberOfLines / 50;
+                    tooltip.GetComponent<ToolTip>().ToolTipText =
+                        "Name: " + entry.name + "\n loc: " + entry.numberOfLines;
+                }
+                if (button.Equals("methods"))
+                {
+                    color = Color.yellow;
+                    metric = entry.numberOfMethods;
+                    tooltip.GetComponent<ToolTip>().ToolTipText =
+                        "Name: " + entry.name + "\n Methods: " + entry.numberOfMethods;
+
+                }
+                else if (button.Equals("abstract_classes"))
+                {
+                    color = Color.cyan;
+                    metric = entry.numberOfAbstractClasses;
+                    tooltip.GetComponent<ToolTip>().ToolTipText =
+                        "Name: " + entry.name + "\n Abstracts: " + entry.numberOfAbstractClasses;
+                }else if (button.Equals("interfaces"))
+                {
+                    color = Color.magenta;
+                    metric = entry.numberOfInterfaces;
+                    tooltip.GetComponent<ToolTip>().ToolTipText =
+                        "Name: " + entry.name + "\n Interfaces: " + entry.numberOfInterfaces;
+                }
+                
+                building.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = color;
+
+                //float scale_w = entry.w / entry.parentEntry.goc.Rows;
+                float scale_w = 0.15f;
+                //float scale_h = entry.h / entry.parentEntry.files.Count / entry.parentEntry.goc.Rows;
+                float scale_h = 0.15f;
+                float x = entry.parentEntry.x + scale_w * entry.parentEntry.files.Count / rows;
+                float y = entry.parentEntry.z + scale_h * (entry.parentEntry.files.Count % rows);
+                building.transform.localScale = new Vector3(scale_w, metric,scale_h);
+                
+                Vector3 parentScale = building.transform.parent.localScale;
+                Vector3 scale = tooltip.transform.localScale;
+                tooltip.transform.localScale = new Vector3 (scale.x / parentScale.x, scale.y / parentScale.y, scale.z / parentScale.z);
+
+                Vector3 posTmp = tooltip.transform.localPosition;
+                posTmp.x = tooltip.transform.localPosition.x;
+                posTmp.y = building.transform.localPosition.y;
+                posTmp.z = tooltip.transform.localPosition.z;
+                tooltip.transform.localPosition = posTmp;
+            }
+        }
     }
+
 }
